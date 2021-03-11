f49b12758395af18bee7c13b127e1334e3506b3f
lc <1033581609@qq.com>
2021-03-11 14:34:19 +0800

    add: 解析深度/嵌套依赖关系

--- demo01-babel/deps_2.ts
+++ demo01-babel/deps_2.ts
@@ -1,29 +1,18 @@
-// 使用 node -r ts-node/register 文件路径 来运行，
+// 然后使用 node -r ts-node/register 文件路径 来运行，
 // 如果需要调试，可以加一个选项 --inspect-brk，再打开 Chrome 开发者工具，点击 Node 图标即可调试
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
 import { readFileSync } from "fs";
 import { resolve, relative, dirname } from "path";
-/* 
-代码思路
-1. 调用collectCodeAndDeps("index.js")
-2. 先把 depRelation['index.js'] 初始化为 { deps: [], code: 'index.js的源码' }
-3. 然后把 index.js 源码 code 变成 ast
-4. 遍历 ast，看看 import 了哪些依赖，假设依赖了 a.js 和 b.js
-5. 把 a.js 和 b.js 写到 depRelation['index.js'].deps 里
-6. 最终得到的 depRelation 就收集了 index.js 的依赖
-
-*/
 
 // 设置根目录
-const projectRoot = resolve(__dirname, "project_1");
-// 类型声明，依赖关系
+const projectRoot = resolve(__dirname, "project_2");
+// 类型声明
 type DepRelation = { [key: string]: { deps: string[]; code: string } };
 // 初始化一个空的 depRelation，用于收集依赖
 const depRelation: DepRelation = {};
 
 // 将入口文件的绝对路径传入函数，如 D:\demo\fixture_1\index.js
-// 收集源代码和依赖
 collectCodeAndDeps(resolve(projectRoot, "index.js"));
 
 console.log(depRelation);
@@ -37,7 +26,6 @@ function collectCodeAndDeps(filepath: string) {
   depRelation[key] = { deps: [], code: code };
   // 将代码转为 AST
   const ast = parse(code, { sourceType: "module" });
-  console.log(ast);
   // 分析文件依赖，将内容放至 depRelation
   traverse(ast, {
     enter: path => {
@@ -48,6 +36,8 @@ function collectCodeAndDeps(filepath: string) {
         const depProjectPath = getProjectPath(depAbsolutePath);
         // 把依赖写进 depRelation
         depRelation[key].deps.push(depProjectPath);
+        // 分析依赖文件的依赖
+        collectCodeAndDeps(depAbsolutePath);
       }
     },
   });
